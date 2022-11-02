6d108d987e83a61841bae2c9c00c9adf70d705e4
Tarun Kumar Sukhu <tksukhu@gmail.com>
2022-11-02 15:42:52 +0530

    update

--- apps/svelteverse/src/routes/Metaverse.svelte
+++ apps/svelteverse/src/routes/Metaverse.svelte
@@ -1,13 +1,14 @@
 <meta-scene>
   <template slot="scene">
-    <metaverse-bootstrap-space />
+    <!-- space -->
+    <a-sky hide-on-ar color="#80d4ff"><!-- --></a-sky>
   </template>
   <template slot="avatar">
     <meta-ready-player-me
       avatarurl="./visitor.glb"
       avatarname="Visitor"
       position="0 -0.65 0"
-      rotation="0 180 0"
-    />
-  </template>
-</meta-scene>
+      rotation="0 180 0"><!-- --></meta-ready-player-me
+    >
+  </template></meta-scene
+>
